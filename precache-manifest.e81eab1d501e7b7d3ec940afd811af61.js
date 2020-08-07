9d3ecdc8fb91063f4032b23f5bf8f739830645ae
Arsen Poghosyan <arsenpoghosyan1989@gmail.com>
2020-08-07 20:21:43 +0400

    Updates

---
+++ precache-manifest.e81eab1d501e7b7d3ec940afd811af61.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762c1a953d25f702806bb854744be4cf",
+    "url": "/Redux-BookStore/index.html"
+  },
+  {
+    "revision": "776cd6c47058f5e60ce6",
+    "url": "/Redux-BookStore/static/css/main.6a785951.chunk.css"
+  },
+  {
+    "revision": "276afc4a5ac114c12e4f",
+    "url": "/Redux-BookStore/static/js/2.6e00c86e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-BookStore/static/js/2.6e00c86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776cd6c47058f5e60ce6",
+    "url": "/Redux-BookStore/static/js/main.57ab71aa.chunk.js"
+  },
+  {
+    "revision": "44e10fe842f22f6ba834",
+    "url": "/Redux-BookStore/static/js/runtime-main.663c45ff.js"
+  },
+  {
+    "revision": "697d269133c021cd2ea483437c765d68",
+    "url": "/Redux-BookStore/static/media/1.697d2691.jpg"
+  },
+  {
+    "revision": "d282bc8b01330936269ff4f4666d3f5b",
+    "url": "/Redux-BookStore/static/media/2.d282bc8b.jpg"
+  },
+  {
+    "revision": "be0e55276dd0a823865f22d3d97d198a",
+    "url": "/Redux-BookStore/static/media/3.be0e5527.jpg"
+  },
+  {
+    "revision": "b5e5b564c7ec894f78d50ae0a1bb8483",
+    "url": "/Redux-BookStore/static/media/4.b5e5b564.jpg"
+  },
+  {
+    "revision": "60766fe1849ff2248c78448bc576db3b",
+    "url": "/Redux-BookStore/static/media/5.60766fe1.jpg"
+  },
+  {
+    "revision": "b461b11e7238d567c1a369f87d254a94",
+    "url": "/Redux-BookStore/static/media/6.b461b11e.jpg"
+  },
+  {
+    "revision": "bc4423131e95545fcb263767166958aa",
+    "url": "/Redux-BookStore/static/media/spinner.bc442313.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Redux-BookStore.git/precache-manifest.c1144b37176ccef35bedf93250c61315.js"
+  "/Redux-BookStore/precache-manifest.e81eab1d501e7b7d3ec940afd811af61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Redux-BookStore.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Redux-BookStore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
